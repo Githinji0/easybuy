06ecba5fba7d579ad1f09f8d60acacd5b20d1365
William Githinji <146653695+Githinji0@users.noreply.github.com>
2026-03-01 23:25:58 +0300

    bn

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,6 +20,11 @@ export const Navbar = () => {
                 </div>
             </div>
             <div className=""></div>
+        <button className="md:hidden p-4 text-text-primary">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+        </button>
         </nav>
     )
 }
