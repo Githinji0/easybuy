6fc2d7ce97d1e0572ce2b830fb397f81499953ce
William Githinji <146653695+Githinji0@users.noreply.github.com>
2026-03-02 22:09:33 +0300

    feat: implement Stripe integration for product listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
-import Image from "next/image";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { stripe } from "@/lib/stripe";
 
-export default function Home() {
+
+export default async function Home() {
+
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 5,
+  })
+  console.log(products)
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       
