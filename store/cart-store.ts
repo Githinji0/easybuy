709f05a691fe0ca57f35f9d4e1e51ff996607bd9
William Githinji <146653695+Githinji0@users.noreply.github.com>
2026-03-05 20:32:54 +0300

    refactor: improve code formatting and structure in cart store

--- store/cart-store.ts
+++ store/cart-store.ts
@@ -1,34 +1,40 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import {create} from "zustand";
+import { create } from "zustand";
 
-import {persist} from "zustand/middleware";
-
+import { persist } from "zustand/middleware";
 
 export interface CartItem {
-    id: string;
-    image: string | null;
-    quantity: number;
-    name: string;
-    price: number
+  id: string;
+  image: string | null;
+  quantity: number;
+  name: string;
+  price: number;
 }
 
 export interface CartStore {
-    items: CartItem[];
-    addItem: (item: CartItem) => void;
-    removeItem: (id: string) => void;
-    clearCart: () => void;
+  items: CartItem[];
+  addItem: (item: CartItem) => void;
+  removeItem: (id: string) => void;
+  clearCart: () => void;
 }
 
-
-export const useCartStore = create<CartStore>()((persist((set) => ({
+export const useCartStore = create<CartStore>()(
+  persist((set) => ({
     items: [],
-    addItem: (item: CartItem) => set((state : CartStore) => ({
-        items: [...state.items, item]
-    })),
-    removeItem: (id: string) => set((state: CartStore) => ({
-        items: state.items.filter(item => item.id !== id)
-    }))
-})), {
-    name: "cart-storage",
-    getStorage: () => localStorage
-}));
+    addItem: (item) =>
+      set((state) => {
+        const exisistingItem = state.items.find((i) => i.id === item.id);
+        if (exisistingItem) {
+          return {
+            items: state.items.map((i) =>
+              i.id === item.id
+                ? { ...i, quantity: i.quantity + item.quantity }
+                : i,
+            ),
+          };
+        }
+
+        return { items: [...state.items, item] };
+      }),
+  })),
+);
