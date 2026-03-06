0389852160b72100a17e171e2e46bd6f9b4b379e
William Githinji <146653695+Githinji0@users.noreply.github.com>
2026-03-06 19:48:31 +0300

    feat: implement cart item handling and display empty cart message in checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,8 +1,24 @@
+"use client";
+
+
+import { useCartStore } from "@/store/cart-store";
+
 export default function CheckoutPage() {
+    const { items } = useCartStore();
+    const totalAmount = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+
+    if (items.length === 0) {
+        return (
+            <div className="container mx-auto p-4">
+                <p>Your cart is empty.</p>
+            </div>
+        );
+    }
     return (
+
         <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">Check out</h1>
-            <p className="text-text-secondary">Explore our wide range of products.</p>
+            <h1 className="text-2xl font-bold mb-4">Checkout</h1>
         </div>
     );
 }
