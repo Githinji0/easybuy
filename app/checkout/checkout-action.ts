f61fa3ee394277c98646b2672fe1a8dc7db1c563
William Githinji <146653695+Githinji0@users.noreply.github.com>
2026-03-06 20:13:52 +0300

    feat: enhance CheckoutPage with item quantity controls and total display; add checkout action handler

---
+++ app/checkout/checkout-action.ts
@@ -0,0 +1,1 @@
+export const checkoutAction = async (formData : FormData): Promise<void> =>  {}
--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 import { useCartStore } from "@/store/cart-store";
+import { Button } from "@/components/ui/button";
+import { checkoutAction } from "./checkout-action";
 
 export default function CheckoutPage() {
-  const { items } = useCartStore();
+  const { items, removeItem, addItem, clearCart } = useCartStore();
   const totalAmount = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0,
@@ -25,16 +27,53 @@ export default function CheckoutPage() {
           <CardTitle className="text-lg font-semibold">Order Summary</CardTitle>
         </CardHeader>
         <CardContent>
-          <ul>{
-            items.map((item) => (
+          <ul>
+            {items.map((item) => (
               <li key={item.id} className="flex justify-between mb-2">
-                <span>{item.name} x {item.quantity}</span>
-                <span>KES {((item.price * item.quantity) / 100).toFixed(2)}</span>
+                <div className="">
+                  <span className="font-medium" >
+                    {item.name} x {item.quantity}
+                  </span>
+                  <span>
+                    KES {((item.price * item.quantity) / 100).toFixed(2)}
+                  </span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Button
+                    variant="outline"
+                    className="rounded-full"
+                    onClick={() => removeItem(item.id)}>
+                    -
+                  </Button>
+                  <span className="px-4">{item.quantity}</span>
+                  <Button
+                    variant="outline"
+                    className="rounded-full"
+                    onClick={() => addItem({...item, quantity: 1})}>
+                    +
+                  </Button>
+                </div>
               </li>
-            ))
-          }</ul>
+            ))}
+          </ul>
+          <div className="border-t pt-4 mt-4">
+            <div className="flex justify-between font-bold text-lg">
+              <span className="text-accent">Total:</span>
+              <span>KES {((totalAmount) / 100).toFixed(2)}</span>
+            </div>
+          </div>
         </CardContent>
       </Card>
+      <form action={checkoutAction}>
+        <Button className="mt-6 w-full mb-10" type="submit" variant={"default"}>
+          Proceed to Payment
+        </Button>
+        <Button variant={"default"} className="mt-6 w-full mb-10" type="submit" onClick={()=>{
+            clearCart();
+        }}>
+            Clear Cart
+        </Button>
+      </form>
     </div>
   );
 }
